151c5de194869a8293134dee18fe97d4c01a9368
Brendanchoi626 <18671@burnside.school.nz>
2020-05-04 15:18:18 +1200

    Have added the portion of contents for the routine, and exercise now page. Some images and good looking layout using css is requied

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template
-import sqlite3
 import os
 import random
-
+import sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    arr = os.listdir(r'C:\Users\brendan\12DTP\12DTP\NoExcuse\static\videos with inspirational qoutes')
+    arr = os.listdir(r'C:\Users\brend\12DTP\12DTP\NoExcuse\static\videos with inspirational qoutes')
     randvid = random.choice(arr)
-    #codes
+    #codesS
     return render_template('home.html', randvid=randvid)
 
 @app.route('/routine/')
 def all_routines():
     #codes
-    return render_template('routine.html')
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Routine;')
+    routines = cur.fetchall()
+    return render_template('routine.html', routines=routines)
 
 @app.route('/help/')
 def help():
@@ -27,9 +30,9 @@ def help():
 def exercisenow(id):
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
-    cur.execute('SELECT Exercise.name, ExerciseRoutine.reps, ExerciseRoutine.sets, ExerciseRoutine.term FROM ExerciseRoutine INNER JOIN Exercise ON ExerciseRoutine.Exercise=Exercise.id WHERE ExerciseRoutine.Routine= (SELECT id FROM Routine WHERE id = {})'.format(id))
-    results = cur.fetchall()
-    return render_template('exercisenow.html', exercisenow=results)
+    cur.execute('SELECT Exercise.name, ExerciseRoutine.reps, ExerciseRoutine.sets, ExerciseRoutine.term FROM ExerciseRoutine INNER JOIN Exercise ON ExerciseRoutine.Exercise=Exercise.id WHERE ExerciseRoutine.Routine= (SELECT id FROM Routine WHERE id = {});'.format(id))
+    exercisenow = cur.fetchall()
+    return render_template('exercisenow.html', exercisenow=exercisenow)
 
 if __name__ == '__main__':
     app.run(debug=True)
